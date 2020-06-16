3588b244d82d2d83e0383d5eee4555e539ae1598
Ardiansyah <ardiansyah@ard333.com>
2020-06-16 15:02:17 +0700

    Remove some option

--- compile.sh
+++ compile.sh
@@ -4,11 +4,8 @@ native-image \
 --verbose \
 --no-server \
 --no-fallback \
---allow-incomplete-classpath \
 --report-unsupported-elements-at-runtime \
--H:+TraceClassInitialization \
--H:+ReportExceptionStackTraces \
--H:ConfigurationFileDirectories=src/main/resources/META-INF/native-image \
 --initialize-at-build-time=net.bytebuddy.description.type,net.bytebuddy.implementation.bind.annotation,net.bytebuddy.ClassFileVersion \
 -jar target/hibernate-search-graalvm-native-image-0.0.1-SNAPSHOT-jar-with-dependencies.jar
+
 
